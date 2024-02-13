d02c9442d02d1d443b1c64df5da751f83a74056c
pverkind <peter.verkinderen@gmail.com>
2024-02-13 08:04:38 +0100

    Add new files for session 2

---
+++ session_2/fixme.py
@@ -0,0 +1,69 @@
+#############
+#           #
+# I N T R O #
+#           #
+#############
+
+# NB= Anything that follows a "#" (hash) in a script is a comment:
+# a comment is meant only for humans to read, the script ignores it.
+
+# This script below contains a lot of mistakes. 
+# It's your task to correct them.
+
+# Check every line to see if it contains a mistake.
+# First "comment out" that line by putting a # before it,
+# and copy the line of code (without the #) to a new line.
+# Make your corrections to the copied line,
+# and explain in a comment what you changed to make the code work!
+
+# Like this:
+
+#print("Hello World!')
+print("Hello World!")   # the quotation marks were unbalanced: " vs '
+
+# Run this script (Run > Run Module; or press F5)
+# to find out if you fixed all mistakes in the script.
+# If something is wrong, Python will stop executing the script
+# and display an error message.
+# The message may be pretty cryptic but at least it will tell you
+# in which line of code it found its first mistake. 
+# NB: sometimes, the mistake is in the line(s) before the error indicated in the message!
+
+# The exercise is ready when it prints the positive message at the end!
+
+# OK, your turn:
+
+#####################
+#                   #
+#  E X E R C I S E  #
+#                   #
+#####################
+
+
+print('Quotation marks are finicky")
+
+print(It's easy to forget them)
+
+print('Sometimes it's hard to find the problem.")
+
+print("Especially when dealing with "embedded" quotes")
+
+Print('Sometimes the problem is not in the string at all')
+
+print(“Always be careful when copying quotation marks from a Word file”)
+
+prınt("Sometimes you have to read REALLY carefully...")
+
+1_word = "great"
+
+three = 1 + "2"
+
+2 + 2 = "four"
+
+len = 20
+
+print(len("abcdefghijklmnopqrstuvwxyz"))
+
+first-words = "mama, dada"
+
+print("DONE! You have fixed all the mistakes")
