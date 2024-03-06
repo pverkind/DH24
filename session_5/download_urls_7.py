357a526dccac4827d6f3035c5c6ac98ab8d3a74a
pverkind <peter.verkinderen@gmail.com>
2024-03-06 10:22:27 +0100

    Add downloads files

---
+++ session_5/download_urls_7.py
@@ -0,0 +1,23 @@
+# In this file, you'll download the PDF files
+# linked in section 6.7 of Maxim Romanov's course site:
+# https://maximromanov.github.io/dh_in_mes/lesson-06-webscraping.html#practicing-scraping
+
+# import the necessary libraries here:
+
+
+
+# paste the HTML of the relevant section
+# that contains the URLS here
+# (Use "inspect" in your browser to see the HTML):
+
+list_of_links = """
+
+"""
+
+# Loop through the list of links to download each:
+# (as file names: use the file names in the URLs)
+
+
+
+
+
---
+++ session_5/looping_downloads.py
@@ -0,0 +1,26 @@
+# import the requests library here:
+
+
+
+# copy and paste our download_file function here:
+
+
+
+
+
+
+
+
+urls = [
+    "https://unicode.org/charts/PDF/U0000.pdf",
+    "https://unicode.org/charts/PDF/U0080.pdf",
+    "https://unicode.org/charts/PDF/U0100.pdf",
+    "https://unicode.org/charts/PDF/U0180.pdf",
+    "https://unicode.org/charts/PDF/U0000.pdf",
+    "https://unicode.org/charts/PDF/U02B0.pdf"
+    ]
+
+# write a loop that downloads each of these pdf files.
+# For the file name of each pdf: use the last element in the URL
+# (U0000.pdf for the first file, U0080.pdf for the next)
+
